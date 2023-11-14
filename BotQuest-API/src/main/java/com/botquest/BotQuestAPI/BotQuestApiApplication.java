508866587e65db1c9d46e5f7342b8bc7f4dccffd
juliaathar <47447272809@info.local>
2023-11-13 21:37:10 -0300

    :tada: init

---
+++ BotQuest-API/src/main/java/com/botquest/BotQuestAPI/BotQuestApiApplication.java
@@ -0,0 +1,13 @@
+package com.botquest.BotQuestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotQuestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotQuestApiApplication.class, args);
+	}
+
+}
