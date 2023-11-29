c5946cf9911a0db40e51426271a92acf46218abb
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-11-29 20:03:13 -0300

    :test_tube: test: Classes de Usuário prontas para teste

--- BotQuest-API/src/main/java/com/botquest/BotQuestAPI/models/TipoUsuarioModel.java
+++ BotQuest-API/src/main/java/com/botquest/BotQuestAPI/models/TipoUsuarioModel.java
@@ -1,29 +1,14 @@
 package com.botquest.BotQuestAPI.models;// Importa as anotações necessárias do JPA e Lombok
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+public enum TipoUsuarioModel {
+    ADMIN("Admin"),
+    COMUM("Comum");
 
-import java.io.Serial;
-import java.io.Serializable;
-import java.util.UUID;
+    private String tipo;
+    TipoUsuarioModel (String tipo) {
+        this.tipo = tipo;
+    }
 
-// Marca a classe como uma entidade JPA
-@Getter
-@Setter
-@Entity
-// Especifica o nome da tabela no banco de dados
-@Table(name = "tb_tipoUsuario")
-public class TipoUsuarioModel implements Serializable {
-    @Serial
-    private static final long serialVersion = 1L;
-    // Define a chave primária da entidade
-    @Id
-    // Configura a estratégia de geração automática do ID
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    // Especifica o nome da coluna no banco de dados
-    @Column(name = "id", nullable = false)
-    private UUID id;
-
-    // Atributo da entidade
-    String nome;
+    public String getRole() {
+        return tipo;
+    }
 }
--- BotQuest-API/src/main/java/com/botquest/BotQuestAPI/models/UsuarioModel.java
+++ BotQuest-API/src/main/java/com/botquest/BotQuestAPI/models/UsuarioModel.java
@@ -4,10 +4,15 @@ import com.botquest.BotQuestAPI.models.TipoUsuarioModel;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 // Marca a classe como uma entidade JPA
@@ -16,7 +21,8 @@ import java.util.UUID;
 @Entity
 // Especifica o nome da tabela no banco de dados
 @Table(name = "tb_usuario")
-public class UsuarioModel implements Serializable {
+
+public class UsuarioModel implements Serializable, UserDetails {
     @Serial
     private static final long serialVersion = 1L;
     // Define a chave primária da entidade
@@ -43,4 +49,48 @@ public class UsuarioModel implements Serializable {
     @OneToOne
     @JoinColumn(name="id_setor", referencedColumnName = "id")
     private SetorModel setorModel;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.tipoUsuarioModel == TipoUsuarioModel.ADMIN){
+            return List.of(
+                    new SimpleGrantedAuthority("ROLE_ADMIN"),
+                    new SimpleGrantedAuthority("ROLE_COMUM")
+            );
+        } else if (this.tipoUsuarioModel == TipoUsuarioModel.COMUM) {
+            return List.of(new SimpleGrantedAuthority("ROLE_COMUM"));
+        }
+
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
